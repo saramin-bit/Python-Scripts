3c5451a5f6ac4af81f6d1fb10ec44970834f6132
spantheslayer <supriyosarkar282@gmail.com>
2020-10-21 17:30:49 +0530

    added google scrapper script

---
+++ google scrapper/google-scrapper.py
@@ -0,0 +1,13 @@
+# this script extracts all the ingredients present on the google home page 
+# also anyone can extract any data by changing the URL 
+
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("http://www.google.com")
+soup = BeautifulSoup(response.text, "html.parser")
+
+links = soup.find_all('a')
+
+for link in links:
+    print(link.get_text())
