82b88682d28707f769a5bca288478279640a6661
saxocat <saxocat@protonmail.com>
2020-10-24 23:15:02 +0100

    Added fibonacci sequence script

---
+++ Fibonacci-Sequence/main.py
@@ -0,0 +1,13 @@
+# Simple fibonacci function
+
+def fibonacci(max_value):
+  """ Prints Fibonacci numbers until max value """
+  a, b = 0, 1
+  while a < max_value:
+    print(a, end = " ")
+    a, b = b, a+b
+
+fibonacci(100)
+
+# You can find more details here: 
+# en.wikipedia.org/wiki/Fibonacci_number
