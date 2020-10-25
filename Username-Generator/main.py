0bb8b602f0acd560e6cc1538e2a4d79b56279ee7
João <73019845+saxocat@users.noreply.github.com>
2020-10-25 15:32:53 +0000

    Update main.py
    Added main.py code!

--- Username-Generator/main.py
+++ Username-Generator/main.py
@@ -1,1 +1,15 @@
 # Username generator like reddit
+import random
+
+with open("wordlist.txt", "r") as f:
+    words = f.read().split("\n")
+
+def user_gen(usernames_number = 1):
+    """ Generates a random username (reddit style) """
+    for i in range(usernames_number):
+        name1 = random.choice(words).title()
+        name2 = random.choice(words).title()
+        str_number = str(random.randint(1, 100))    
+        print(f"{name1}{name2}{str_number}")
+
+user_gen(10) # Specify the number of usernames you want with *usernames_number* arg
